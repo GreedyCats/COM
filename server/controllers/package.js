ce6b1cff2cb9cf6a38a2cec0fdead4c5f5914635
kevin14 <kevin14@me.com>
2015-07-13 15:12:19 +0800

    新增package假数据

--- server/controllers/package.js
+++ server/controllers/package.js
@@ -9,13 +9,11 @@ module.exports = {
 			next(err,data);
 		});
 	},
-	addOneRelationshipOfPackageAndProduct:function(){
-		var packageID_1 = '55a3336db1146c4873b45b6a'; //一个默认测试PackageID
-		var packageID_2 = '55a349c186a985f37487fc0d'; //一个默认测试PackageID
-		var packageID_3 = '55a349e54c9f663b75868027'; //一个默认测试PackageID
-
-		
-
+	addOneRelationshipOfPackageAndProduct:function(data,next){
+		var newRelation = new PackageAndProduct(data);
+		newRelation.save(function(err,data){
+			next(err,data);
+		});
 	},
 	getTodayPackages:function(next){
 		Package.find().select('_id title weight country').populate({path:'country',select:'name flagUrl'}).exec(next);
--- server/routes/package.js
+++ server/routes/package.js
@@ -8,32 +8,106 @@ router.get('/', function(req, res) {
 });
 
 router.get('/addOneTest', function(req, res) {
-	packages.addOne({
+    packages.addOne({
         'title': 'Summer Tea 夏日么么茶',
-        'subTitle':'夏日么么茶下午茶系列',
-        'price':68,
-        'originalPrice':86,
-        'packageImage':'/uploads/1.jpeg'
-    },function(err,data){
-    	if (err) {
-    		res.send(500);
-    	}else{
-    		var data = JSON.stringify(data);
-    		res.send(data);
-    	}
+        'subTitle': '夏日么么茶下午茶系列',
+        'price': 68,
+        'originalPrice': 86,
+        'packageImage': '/uploads/1.jpeg'
+    }, function(err, data) {
+        if (err) {
+            res.send(500);
+        } else {
+            var data = JSON.stringify(data);
+            res.send(data);
+        }
     })
 });
 
-router.get('/getAll', function(req, res) {
-	packages.getAll(function(err,data){
-    	if (err) {
-    		res.send(500);
-    	}else{
-    		var data = JSON.stringify(data);
-    		res.send(data);
-    	}
+router.get('/getTodayPackage', function(req, res) {
+    packages.getAll(function(err, data) {
+        if (err) {
+            res.send(500);
+        } else {
+            var data = JSON.stringify(data);
+            res.send(data);
+        }
     })
+});
+
+router.get('/addOneRelation', function(req, res) {
+
+    var data = [];
+
+    var packageID_1 = '55a3336db1146c4873b45b6a'; //一个默认测试PackageID
+    var packageID_2 = '55a349c186a985f37487fc0d';
+    var packageID_3 = '55a349e54c9f663b75868027';
+
+    var productID_1 = '55a318ee7b484bef6fa76bad';
+    var productID_2 = '55a318f1e06032f36fb8a1a8';
+    var productID_3 = '55a318f66bc37df46f07ca74';
+    var productID_4 = '55a319092707d2f96f5ac2a6';
+
+    var r1 = {
+        package: packageID_1,
+        product: productID_1,
+        count: 1
+    }
+
+    var r2 = {
+        package: packageID_1,
+        product: productID_2,
+        count: 1
+    }
+
+    var r3 = {
+        package: packageID_1,
+        product: productID_3,
+        count: 1
+    }
+
+    var r4 = {
+        package: packageID_1,
+        product: productID_4,
+        count: 1
+    }
+
+    var r5 = {
+        package: packageID_2,
+        product: productID_1,
+        count: 1
+    }
+
+    var r6 = {
+        package: packageID_2,
+        product: productID_2,
+        count: 1
+    }
+
+    var r7 = {
+        package: packageID_2,
+        product: productID_3,
+        count: 1
+    }
+
+    var r8 = {
+        package: packageID_2,
+        product: productID_4,
+        count: 1
+    }
+
+    data = [r1, r2, r3, r4, r5, r6, r7, r8];
+
+    data.forEach(function(r, i) {
+        packages.addOneRelationshipOfPackageAndProduct(r, function(err, data) {
+        	if (err) {
+        		console.log(err);
+        	};
+        });
+    });
+
+    res.send('true');
+
 });
 
 module.exports = router;
-
