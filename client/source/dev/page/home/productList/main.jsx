2602f1c5c3d4fbef9d4009c3625f3b2518661570
zhiqingz <910202320@qq.com>
2015-07-13 15:38:31 +0800

    商品列表title限制为2行，多余做溢出处理

--- client/source/dev/page/home/productList/main.jsx
+++ client/source/dev/page/home/productList/main.jsx
@@ -24,7 +24,7 @@ define([
 				countryFlag: '/uploads/korea.jpg',
 				count: 2
 			},{
-				title: 'SANRITSU三立 韩国进口巧克力夹心饼干',
+				title: '三立 Sanritsu 日本进口零食品 白巧克力曲奇夹心薄酥饼干96g12枚 糕点独立包装鲜美的白巧克力夹心，甜而不腻，绵软酥脆',
 				weight: '92.4g',
 				thumbnail: '/uploads/productImage1.jpg',
 				countryFlag: '/uploads/korea.jpg',
