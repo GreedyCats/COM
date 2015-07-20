e59cc7ff1f47b838f21b5cb0f28f68f52821eb59
zhiqingz <910202320@qq.com>
2015-07-20 17:01:43 +0800

    detail页面的创建

---
+++ client/source/dev/page/detail/main.jsx
@@ -0,0 +1,55 @@
+define([
+	'when',
+	'React',
+	'jQuery',
+	'less!./detail'
+], function(when, React, $) {
+	return React.createClass({
+		getInitialState: function(){
+			return {}
+		},
+		render: function(){
+			return (
+				<div className='pageDetail'>
+					<div className='topBox'>
+						<img className='topImage' src='/uploads/productImage2.png' />
+						<p className='title'>
+							三立 韩国进口巧克力夹心饼干
+						</p>
+						<p className='subTitle'>
+							Lorem ipsum dolor sit amet, consectetur
+						</p>
+						<div className='topOtherInfo'>
+							<div className='leftBox'>
+								<img className='countryFlag' src='/uploads/korea.jpg' />
+								<span className='weight'>92.4g/盒</span>
+							</div>
+							<div className='rightBox'>
+								<span>原价：</span>
+								<del>¥<span>68.0</span></del>
+							</div>
+						</div>
+					</div>
+					<div className='middleLine'></div>
+					<div className='bottomBox'>
+						<ul className='tabList'>
+							<li className='tabItem active'>
+								<p className='tabTitle'>规格参数</p>
+								<p className='tabSubtitle'>Specification</p>
+							</li>
+							<li className='tabItem'>
+								<p className='tabTitle'>规格参数</p>
+								<p className='tabSubtitle'>Specification</p>
+							</li>
+							<li className='tabItem'>
+								<p className='tabTitle'>规格参数</p>
+								<p className='tabSubtitle'>Specification</p>
+							</li>
+							<div className='justifyline'></div>
+						</ul>
+					</div>
+				</div>
+			);
+		}
+	});
+});
